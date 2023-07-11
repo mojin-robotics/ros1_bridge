f27d98c049d12ada0e7f78e818951188aab0b3d8
fmessmer <felixmessmer@gmail.com>
2023-07-11 14:34:13 +0200

    fix run_tests

--- launch/amr_action_bridge_launch.py
+++ launch/amr_action_bridge_launch.py
@@ -1,3 +1,18 @@
+# Copyright 2023 4am Robotics GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
@@ -10,20 +25,26 @@ def generate_launch_description():
                 executable='action_bridge',
                 output='screen',
                 name='action_bridge_execute_transferjob',
-                namespace='action_bridge_execute_transferjob',  # corresponds to setting argument "--ros-args --remap __name:=action_bridge_execute_transferjob"
+                namespace='action_bridge_execute_transferjob',
                 respawn=True,
                 respawn_delay=1,
-                arguments=["ros2", "amr_road_network_msgs", "action/ExecuteTransferJob", "/execute_transferjob"]
+                arguments=['ros2',
+                           'amr_road_network_msgs',
+                           'action/ExecuteTransferJob',
+                           '/execute_transferjob']
             ),
             Node(
                 package='ros1_bridge',
                 executable='action_bridge',
                 output='screen',
                 name='action_bridge_execute_transferjob_flexbe',
-                namespace='action_bridge_execute_transferjob_flexbe',  # corresponds to setting argument "--ros-args --remap __name:=action_bridge_execute_transferjob_flexbe"
+                namespace='action_bridge_execute_transferjob_flexbe',
                 respawn=True,
                 respawn_delay=1,
-                arguments=["ros1", "amr_road_network_msgs", "ExecuteTransferJob", "/execute_transferjob_flexbe"]
+                arguments=['ros1',
+                           'amr_road_network_msgs',
+                           'ExecuteTransferJob',
+                           '/execute_transferjob_flexbe']
             ),
         ]
     )
